83e081daa5d1f4b56972b536ca298f831b3bf0a5
avivsarig <avivs15@gmail.com>
2025-06-05 07:27:57 +0200

    fix: minor import and logger calls fixes

--- app/src/domain/task_processor.py
+++ app/src/domain/task_processor.py
@@ -1,16 +1,24 @@
+import logging
 from datetime import datetime
 
-from classes import ArchiveItem, TaskItem
 from croniter import croniter
-from logger import LoggingMixin
-from vault_manager import VaultManager
+
+from app.src.domain.entities import ArchiveItem, TaskItem
+from app.src.infrastructure.vault_manager import VaultManager
+
+logger = logging.getLogger(__name__)
 
 
-class TaskProcessor(LoggingMixin):
-    def process_active_task(self, vault: VaultManager, task: TaskItem, config):
-        self.logger.info(f"Processing active task: {task.title}")
+class TaskProcessor:
+    def process_active_task(
+        self,
+        vault: VaultManager,
+        task: TaskItem,
+        config,
+    ):
+        logger.info(f"Processing active task: {task.title}")
         if task.done and task.repeat_task:
-            self.logger.info("Task is done and repeating - resetting")
+            logger.info("Task is done and repeating - resetting")
             self.reset_repeating_task(
                 task=task,
                 vault=vault,
@@ -20,28 +28,28 @@ class TaskProcessor(LoggingMixin):
 
         # if done - update completed_at
         if task.done and not task.completed_at:
-            self.logger.info("Task is done - filling completion date")
+            logger.info("Task is done - filling completion date")
             task.completed_at = datetime.now()
 
         # if completed_at but not done - clear completed_at
         if task.completed_at and not task.done:
-            self.logger.info("Task not done - clearing completion date")
+            logger.info("Task not done - clearing completion date")
             task.completed_at = None
 
         # if done and has completed_at - move to completed tasks dir
         if task.done and task.completed_at:
-            self.logger.info("Task is done - moving to completion cache")
+            logger.info("Task is done - moving to completion cache")
             vault.move_note(item=task, destination_dir=config["completed_tasks"])
 
         # is do_date expired?
         # TODO: datetime is invalid for some reason
         # e.g.: do_date = 2025-04-01, of type <class 'datetime.date'>
         if not isinstance(task.do_date, datetime):
-            self.logger.info("Do date is invalid - setting for today")
-            self.logger.info(f"do_date = {task.do_date}, of type {type(task.do_date)}")
+            logger.info("Do date is invalid - setting for today")
+            logger.info(f"do_date = {task.do_date}, of type {type(task.do_date)}")
             task.do_date = datetime.today().strftime("%Y-%m-%d")
         elif (task.do_date.date() - datetime.today().date()).days < 0:
-            self.logger.info("Do date have passed - setting for today")
+            logger.info("Do date have passed - setting for today")
             task.do_date = datetime.today().strftime("%Y-%m-%d")
 
         vault.write_note(task, subfolder=config["tasks"])
@@ -53,22 +61,22 @@ class TaskProcessor(LoggingMixin):
         config,
         retent_for_days,
     ):
-        self.logger.info(f"Processing completed task: {task.title}")
+        logger.info(f"Processing completed task: {task.title}")
 
         # if done but no completed_at - update completed_at
         if task.done and not task.completed_at:
-            self.logger.info("No completion date - updating to now")
+            logger.info("No completion date - updating to now")
             task.completed_at = datetime.now()
 
         # if completed_at but not done - reactivate
         if task.completed_at and not task.done:
-            self.logger.info("Task not done - reactivating")
+            logger.info("Task not done - reactivating")
             task.completed_at = None
             vault.move_note(item=task, destination_dir=config["tasks"])
 
         # make sure is_project is up to date
         if (task.content == "") == task.is_project:
-            self.logger.info(
+            logger.info(
                 f"Task has {'no' if not task.content else ''} "
                 "content - is_project changed to {not task.is_project}"
             )
@@ -80,11 +88,11 @@ class TaskProcessor(LoggingMixin):
             and isinstance(task.completed_at, datetime)
             and (datetime.now() - task.completed_at).days > retent_for_days
         ):
-            self.logger.info(
+            logger.info(
                 f"Task completed {(datetime.now() - task.completed_at).days} days ago"
             )
             if not task.is_project:
-                self.logger.info("Deleting over-retented task")
+                logger.info("Deleting over-retented task")
                 vault.delete_note(task)
             else:
                 self.archive_task(
@@ -99,7 +107,7 @@ class TaskProcessor(LoggingMixin):
         task: TaskItem,
         config,
     ):
-        self.logger.info("Archiving task")
+        logger.info("Archiving task")
         callout = "> [!Example] Task properties"
         for k, v in task.frontmatter.items():
             callout = f"{callout}\n> {k}: {v}"
